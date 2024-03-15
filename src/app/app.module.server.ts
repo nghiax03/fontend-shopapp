83eed2850923ef079feee29948f1aaacfe51a005
nghiax03 <nghiabn8622@gmail.com>
2024-03-15 16:32:36 +0700

    rework font-end

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [DetailProductComponent],
 })
 export class AppServerModule {}
